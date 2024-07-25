55573840540f9c9f6d02e0919044bbc5e99788e9
Thomas E. Enebo <tom.enebo@gmail.com>
2024-07-25 11:27:22 -0400

    more descriptive error when trying to run specs when you haven't built Java bit yet

--- spec/helpers/load_dirgra.rb
+++ spec/helpers/load_dirgra.rb
@@ -2,7 +2,9 @@ files = Dir["#{__dir__}/../../target/*.jar"].find_all do |file|
   file !~ /source|javadoc/
 end
 
-if files.length != 1
+if files.length == 0
+    exit "You have no dirgra releases in target.  Please run `mvn package`"
+elsif files.length != 1
   exit "You have multiple dirgra releases in target.  Please clean up your workspace"
 end
 
